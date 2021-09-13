890888c3112fed2636c4067a9e97298cb80fd0c3
TowsifHasan <towsiafehasan007@gmail.com>
2021-09-13 18:35:52 +0600

    Final Commit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 const loadProducts = () => {
-  // const url = `https://fakestoreapi.com/products`;
-  // fetch(url)
-  fetch(`../js/data.json`)
+  const url = `https://fakestoreapi.com/products`;
+  fetch(url)
     .then((response) => response.json())
     .then((data) => showProducts(data));
 };
